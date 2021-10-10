a03b515834c81d84a87562dfea91363befba2750
Blare <yc927596@dal.ca>
2021-10-10 12:48:01 -0400

    Add function to upload image from url.

--- app/user.py
+++ app/user.py
@@ -4,8 +4,12 @@ from .MySQL_DB import mysql
 import bcrypt
 import MySQLdb
 import os
+import urllib.request
+import validators
+from urllib.parse import urlparse
 from .utils import login_required
 from werkzeug.utils import secure_filename
+from wand.image import Image
 
 
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'bmp'])
@@ -65,8 +69,27 @@ def upload(user_name):
             fname = secure_filename(f.filename)
             image = '/static/upload/{}'.format(fname)
             print('filename - {}'.format(image))
+            #blurname = 'blur_{}'.format(fname)
+            #shadename = 'shade_{}'.format(fname)
+            #spreadname = 'spread_{}'.format(fname)
             filepath = os.path.join(IMAGE_UPLOAD, fname)
             f.save(filepath)
+            #with Image(filename=fname) as original:
+            #    with original.clone() as imgblur:
+            #        imgblur.blur(radius=0, sigma=8)
+            #        imgblur.save(filename=blurname)
+            #        blurpath = os.path.join(IMAGE_UPLOAD, blurname)
+            #        f.save(blurpath)
+            #    with original.clone() as imgshade:
+            #        imgshade.shade(gray=True, azimuth=286.0, elevation=45.0)
+            #        imgshade.save(filename=shadename)
+            #        shadepath = os.path.join(IMAGE_UPLOAD, shadename)
+            #        f.save(shadepath)
+            #    with original.clone() as imgspread:
+            #        imgspread.spread(radius=8)
+            #        imgspread.save(filename=spreadname)
+            #        spreadpath = os.path.join(IMAGE_UPLOAD, spreadname)
+            #        f.save(spreadpath)
         else:
             flash('Wrong image type')
             return render_template('user/user_page.html', user_name=session['user_name'])
@@ -74,11 +97,39 @@ def upload(user_name):
         query = "INSERT INTO image(image_path, user_id) " \
                 "VALUES (%s, %s)"
         cursor.execute(query, (image, session['user_id']))
+        #cursor.execute(query, ('/static/upload/{}'.format(blurname), session['user_id']))
+        #cursor.execute(query, ('/static/upload/{}'.format(shadename), session['user_id']))
+        #cursor.execute(query, ('/static/upload/{}'.format(spreadname), session['user_id']))
         mysql.connection.commit()
         cursor.close()
         return render_template('user/user_page.html', user_name=session['user_name'])
     else:
         return render_template('image/image_upload.html')
+
+
+@webapp.route('/user/<string:user_name>/upload_url', methods=['GET', 'POST'])
+@login_required
+def upload_url(user_name):
+    if request.method == 'POST':
+        url = request.form['image_url']
+        if validators.url(url):
+            file = urlparse(url)
+            filename = os.path.basename(file.path)
+            url_image = "/static/upload/" + filename
+            filepath = os.path.join(IMAGE_UPLOAD, filename)
+            urllib.request.urlretrieve(url, filepath)
+        else:
+            flash('Wrong URL')
+            return render_template('user/user_page.html', user_name=session['user_name'])
+        cursor = mysql.connection.cursor()
+        query = "INSERT INTO image(image_path, user_id) " \
+                "VALUES (%s, %s)"
+        cursor.execute(query, (url_image, session['user_id']))
+        mysql.connection.commit()
+        cursor.close()
+        return render_template('user/user_page.html', user_name=session['user_name'])
+    else:
+        return render_template('image/image_url.html')
 
 
 @webapp.route('/user/<string:user_name>/show', methods=['GET', 'POST'])
