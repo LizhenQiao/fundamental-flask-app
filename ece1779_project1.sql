54af53b12e3b4c9c365f6cba87e352786d56df98
LizhenQiao <948090427@qq.com>
2021-10-13 23:53:57 -0400

    update .sql file

--- ece1779_project1.sql
+++ ece1779_project1.sql
@@ -2,12 +2,12 @@
 --
 -- Host: localhost    Database: ece1779_project1
 -- ------------------------------------------------------
--- Server version	8.0.26
+-- Server version	5.7.27-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8 */;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -23,11 +23,11 @@ DROP TABLE IF EXISTS `admins`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `admins` (
-  `admin_id` int NOT NULL AUTO_INCREMENT,
+  `admin_id` int(11) NOT NULL AUTO_INCREMENT,
   `admin_name` varchar(100) NOT NULL,
   `admin_password` varchar(100) NOT NULL,
   PRIMARY KEY (`admin_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -36,35 +36,35 @@ CREATE TABLE `admins` (
 
 LOCK TABLES `admins` WRITE;
 /*!40000 ALTER TABLE `admins` DISABLE KEYS */;
-INSERT INTO `admins` VALUES (1,'Jiahao Lu','123456'),(2,'Lizhen Qiao','123456'),(3,'Yuchen Dai','123456'),(4,'JiahaoLu','123456');
+INSERT INTO `admins` VALUES (1,'admin1','admin1');
 /*!40000 ALTER TABLE `admins` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `image`
+-- Table structure for table `images`
 --
 
-DROP TABLE IF EXISTS `image`;
+DROP TABLE IF EXISTS `images`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `image` (
-  `image_id` int NOT NULL AUTO_INCREMENT,
+CREATE TABLE `images` (
+  `image_id` int(11) NOT NULL AUTO_INCREMENT,
   `image_path` varchar(100) NOT NULL,
-  `user_id` int NOT NULL,
+  `user_id` int(11) NOT NULL,
   PRIMARY KEY (`image_id`),
   KEY `user_id` (`user_id`),
-  CONSTRAINT `image_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+  CONSTRAINT `images_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `image`
+-- Dumping data for table `images`
 --
 
-LOCK TABLES `image` WRITE;
-/*!40000 ALTER TABLE `image` DISABLE KEYS */;
-INSERT INTO `image` VALUES (1,'/static/upload/test.jpg',5),(2,'/static/upload/u31341568972388082980fm26fmtauto.jpg',5);
-/*!40000 ALTER TABLE `image` ENABLE KEYS */;
+LOCK TABLES `images` WRITE;
+/*!40000 ALTER TABLE `images` DISABLE KEYS */;
+INSERT INTO `images` VALUES (1,'/static/upload/8507971.jpg',1),(2,'/static/upload/blur_8507971.jpg',1),(3,'/static/upload/shade_8507971.jpg',1),(4,'/static/upload/spread_8507971.jpg',1);
+/*!40000 ALTER TABLE `images` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -75,11 +75,12 @@ DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `users` (
-  `user_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
   `user_name` varchar(100) NOT NULL,
   `user_password` varchar(100) NOT NULL,
+  `user_email` varchar(100) NOT NULL,
   PRIMARY KEY (`user_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -88,7 +89,7 @@ CREATE TABLE `users` (
 
 LOCK TABLES `users` WRITE;
 /*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (4,'def','123456'),(5,'abc','$2b$12$.YgpMYb8IU3Il9XNNzFMNecp5hlIw04YnxEAG5SeMNNSYxbk4LCP.'),(6,'ggg','$2b$12$3pv09tC1DUXXHYaYM3LsKuOggzGjwURYNaJy71kNLi.QmWkSgwMgO');
+INSERT INTO `users` VALUES (1,'test1','$2b$12$HEdFyOpRptcOlDTiOWF48uFNSP.M6/mEwSeHHthRaCclMZ4dC4cuO','948090427@qq.com'),(2,'test2','b\'$2b$12$RDAhPshTy2OuqqCozV4qyuBMhCrODx0LmiiSh1VSPWu1U4HWhZpRy\'','');
 /*!40000 ALTER TABLE `users` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -101,4 +102,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-10-06 14:27:05
+-- Dump completed on 2021-10-13 23:51:32
