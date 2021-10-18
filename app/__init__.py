e9a6fffb50cc46aebfd8bc89e4c25c3136131529
LizhenQiao <948090427@qq.com>
2021-10-18 13:41:52 -0400

    modify the place of secret key initializing

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,13 @@
 from flask import Flask
 
 
+
 webapp = Flask(__name__)
+webapp.secret_key = "you-will-never-know-lol"
+webapp.jinja_env.auto_reload = True
+webapp.config['TEMPLATES_AUTO_RELOAD'] = True
+webapp.config['SESSION_TYPE'] = 'filesystem'
+
 
 from app import admin
 from app import main
